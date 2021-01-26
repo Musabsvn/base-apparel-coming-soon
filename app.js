563c9fa1b0e785bf745ff39ed8f75fec6b37d765
Raj <raj.mhatre.official@gmail.com>
2021-01-26 18:20:59 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const mailid = document.querySelector('#email');
+const erroricon = document.querySelector('.error-icon')
+const submitbtn = document.querySelector('#myBtn');
+const errormsg = document.querySelector('.error');
+let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const submit = () => {
+
+    submitbtn.addEventListener('click', () => {
+        if (mailid.value.match(mailformat)) {
+            mailid.style.borderColor = "rgba(128, 128, 128, 0.5)";
+            errormsg.style.display = "none";
+            erroricon.style.display = "none";
+        }
+        else {
+            mailid.style.borderColor = "hsl(0, 93%, 68%)";
+            errormsg.style.display = "block";
+            erroricon.style.display = "inline";
+        }
+    })
+}
+
+submit();
