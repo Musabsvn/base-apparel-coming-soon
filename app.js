fa937686259e629e4add5d4a6c4aa1bb822b197a
Raj <raj.mhatre.official@gmail.com>
2021-01-29 11:10:19 +0530

    form onsubmit fixed

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const submitbtn = document.querySelector('#myBtn');
 const errormsg = document.querySelector('.error');
 let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-const submit = () => {
+var returnval = false;
+function check() {
 
     submitbtn.addEventListener('click', () => {
         if (mailid.value.match(mailformat)) {
             mailid.style.borderColor = "rgba(128, 128, 128, 0.5)";
             errormsg.style.display = "none";
             erroricon.style.display = "none";
+            returnval = true;
         }
         else {
             mailid.style.borderColor = "hsl(0, 93%, 68%)";
             errormsg.style.display = "block";
             erroricon.style.display = "inline";
+            returnval = false;
         }
-    })
+    });
+    return returnval;
 }
 
-submit();
+check();
